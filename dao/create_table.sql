52787ea139469f80f6d82a135884e4a7325d1d77
zq2599 <zq2599@gmail.com>
2016-12-03 10:21:50 +0800

    提交简表语句

---
+++ dao/create_table.sql
@@ -0,0 +1,12 @@
+﻿drop table if EXISTS ext_attr;
+
+CREATE TABLE `ext_attr` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `main_id` bigint DEFAULT NULL,
+  `create_time` bigint DEFAULT NULL,
+  `modify_time` bigint DEFAULT NULL,
+  `name` varchar(128) DEFAULT NULL,
+  `value` varchar(512) DEFAULT NULL,
+  `type` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
