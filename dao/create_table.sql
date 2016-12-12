f010341e06720b1407b0cb54f842b807f36816e0
zq2599 <zq2599@gmail.com>
2016-12-13 00:27:48 +0800

    增加基础库存表的建表语句

--- dao/create_table.sql
+++ dao/create_table.sql
@@ -10,3 +10,18 @@ CREATE TABLE `ext_attr` (
   `type` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+
+--基础库存表
+drop table if EXISTS base_stock;
+
+CREATE TABLE `base_stock` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `create_time` bigint DEFAULT NULL,
+  `modify_time` bigint DEFAULT NULL,
+  `name` varchar(128) DEFAULT NULL,
+  `type` varchar(32) DEFAULT NULL,
+  `max_value` int DEFAULT NULL,
+  `stock_value` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
